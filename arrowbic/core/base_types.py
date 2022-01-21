2736492c076e5d8fdae70ba3b33f0cb2b4a0539b
Paul Balanca <paul.balanca@gmail.com>
2022-01-21 16:13:42 +0000

    Support base Python/Numpy/Arrow types, with conversion methods.

---
+++ arrowbic/core/base_types.py
@@ -0,0 +1,93 @@
+from typing import Dict
+
+import numpy as np
+import pyarrow as pa
+from numpy.typing import DTypeLike
+
+_base_from_arrow_to_python_mapping: Dict[pa.DataType, DTypeLike] = {
+    pa.null(): type(None),
+    pa.bool_(): bool,
+    pa.int8(): int,
+    pa.int16(): int,
+    pa.int32(): int,
+    pa.int64(): int,
+    pa.uint8(): int,
+    pa.uint16(): int,
+    pa.uint32(): int,
+    pa.uint64(): int,
+    pa.float16(): float,
+    pa.float32(): float,
+    pa.float64(): float,
+    pa.string(): str,
+    pa.utf8(): str,
+    pa.binary(length=-1): bytes,
+    # Using the Numpy datetime64 for timestamp/duration.
+    pa.timestamp("s"): np.dtype("datetime64[s]"),
+    pa.timestamp("ms"): np.dtype("datetime64[ms]"),
+    pa.timestamp("us"): np.dtype("datetime64[us]"),
+    pa.timestamp("ns"): np.dtype("datetime64[ns]"),
+    pa.duration("s"): np.dtype("timedelta64[s]"),
+    pa.duration("ms"): np.dtype("timedelta64[ms]"),
+    pa.duration("us"): np.dtype("timedelta64[us]"),
+    pa.duration("ns"): np.dtype("timedelta64[ns]"),
+}
+"""Mapping between supported base types and the Python (or Numpy dtype) equivalent.
+"""
+
+
+def is_supported_base_type(type: pa.DataType) -> bool:
+    """Is the input type a supported base type?
+
+    Args:
+        type: PyArrow type (or Numpy dtype / Python class).
+    Returns:
+        Is it a supported base type?
+    """
+    return type in _base_from_arrow_to_python_mapping
+
+
+def from_numpy_to_arrow_type(dtype: DTypeLike) -> pa.DataType:
+    """Convert a Numpy dtype (or Python base class) to an equivalent Arrow type."""
+    if isinstance(dtype, pa.DataType):
+        return dtype
+
+    # Let's handle a few corner cases!
+    if isinstance(dtype, np.dtype) and dtype == np.dtype("O"):
+        raise TypeError("Can not convert Numpy object dtype to Arrow type.")
+    elif dtype is None or dtype == type(None):  # noqa: E721
+        return pa.null()
+
+    # Rely on PyArrow for the default.
+    return pa.from_numpy_dtype(dtype)
+
+
+def from_arrow_to_numpy_dtype(awtype: pa.DataType) -> DTypeLike:
+    """Convert an Arrow type to an equivalent Numpy dtype."""
+    if isinstance(awtype, np.dtype):
+        return type
+
+    # Corner cases. Weird choices of PyArrow to return dtype('O')
+    if awtype is None or awtype == type(None):  # noqa: E721
+        return type(None)
+    elif pa.types.is_null(awtype):
+        return type(None)
+    elif pa.types.is_string(awtype):
+        return np.dtype(str)
+    elif pa.types.is_binary(awtype):
+        return np.dtype(bytes)
+    elif pa.types.is_timestamp(awtype):
+        # PyArrow converting to "ns"?!
+        return np.datetime64(0, awtype.unit).dtype
+    elif pa.types.is_duration(awtype):
+        # PyArrow converting to "ns"?!
+        return np.timedelta64(0, awtype.unit).dtype
+    return awtype.to_pandas_dtype()
+
+
+def from_arrow_to_python_class(type: pa.DataType) -> DTypeLike:
+    """Convert an Arrow type to an (almost) equivalent Python class.
+
+    For now, Arrowbic is using Numpy dtype for timestamp and duration conversion, to keep
+    the proper time unit information.
+    """
+    return _base_from_arrow_to_python_mapping[type]
---
+++ tests/core/test_base_types.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pyarrow as pa
+import pytest
+
+from arrowbic.core.base_types import from_arrow_to_numpy_dtype, from_arrow_to_python_class, from_numpy_to_arrow_type
+
+
+def test__from_numpy_to_arrow_type__np_dtype__proper_coverage() -> None:
+    assert from_numpy_to_arrow_type(None) == pa.null()
+    assert from_numpy_to_arrow_type(type(None)) == pa.null()
+
+    assert from_numpy_to_arrow_type(np.bool_) == pa.bool_()
+    assert from_numpy_to_arrow_type(np.int8) == pa.int8()
+    assert from_numpy_to_arrow_type(np.float32) == pa.float32()
+
+    assert from_numpy_to_arrow_type(np.dtype(str)) == pa.string()
+    assert from_numpy_to_arrow_type(np.dtype(bytes)) == pa.binary(-1)
+
+    assert from_numpy_to_arrow_type(np.dtype("datetime64[s]")) == pa.timestamp("s")
+    assert from_numpy_to_arrow_type(np.dtype("timedelta64[ns]")) == pa.duration("ns")
+
+    with pytest.raises(TypeError):
+        from_numpy_to_arrow_type(np.dtype("O"))
+
+
+def test__from_numpy_to_arrow_type__python_class__proper_coverage() -> None:
+    assert from_numpy_to_arrow_type(None) == pa.null()
+    assert from_numpy_to_arrow_type(type(None)) == pa.null()
+
+    assert from_numpy_to_arrow_type(bool) == pa.bool_()
+    assert from_numpy_to_arrow_type(int) == pa.int64()
+    assert from_numpy_to_arrow_type(float) == pa.float64()
+
+    assert from_numpy_to_arrow_type(str) == pa.string()
+    assert from_numpy_to_arrow_type(bytes) == pa.binary(-1)
+
+
+def test__from_arrow_to_numpy_dtype__proper_coverage() -> None:
+    assert from_arrow_to_numpy_dtype(None) == type(None)  # noqa: E721
+    assert from_arrow_to_numpy_dtype(type(None)) == type(None)  # noqa: E721
+    assert from_arrow_to_numpy_dtype(pa.null()) == type(None)  # noqa: E721
+
+    assert from_arrow_to_numpy_dtype(pa.bool_()) == np.bool_
+    assert from_arrow_to_numpy_dtype(pa.uint8()) == np.uint8
+    assert from_arrow_to_numpy_dtype(pa.float32()) == np.float32
+
+    assert from_arrow_to_numpy_dtype(pa.string()) == np.dtype(str)
+    assert from_arrow_to_numpy_dtype(pa.binary(-1)) == np.dtype(bytes)
+
+    assert from_arrow_to_numpy_dtype(pa.timestamp("us")) == np.dtype("datetime64[us]")
+    assert from_arrow_to_numpy_dtype(pa.duration("ns")) == np.dtype("timedelta64[ns]")
+
+
+def test__from_arrow_to_python_class__proper_coverage() -> None:
+    assert from_arrow_to_python_class(pa.null()) == type(None)  # noqa: E721
+    assert from_arrow_to_python_class(pa.float32()) == float  # noqa: E721
+    assert from_arrow_to_python_class(pa.int32()) == int  # noqa: E721
+
+    assert from_arrow_to_python_class(pa.string()) == str  # noqa: E721
+    assert from_arrow_to_python_class(pa.binary(-1)) == bytes  # noqa: E721
+
+    assert from_arrow_to_python_class(pa.timestamp("us")) == np.dtype("datetime64[us]")
+    assert from_arrow_to_python_class(pa.duration("ns")) == np.dtype("timedelta64[ns]")
